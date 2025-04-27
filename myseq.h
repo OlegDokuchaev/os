c6c55eedfcdab2956d0c4c9f0f9eb930fd88216b
OlegDokuchaev <58179097+OlegDokuchaev@users.noreply.github.com>
2025-04-27 22:14:08 +0300

    Update myseq.h

--- myseq.h
+++ myseq.h
@@ -5,13 +5,13 @@
 #include <linux/fs.h>
 #include <linux/printk.h>
 
-int     my_single_open(struct file *file,
+extern int     my_single_open(struct file *file,
                        int (*show)(struct seq_file *, void *),
                        void *data);
-ssize_t my_seq_read(struct file *file, char __user *buf,
+extern ssize_t my_seq_read(struct file *file, char __user *buf,
                     size_t size, loff_t *ppos);
-int     my_single_release(struct inode *inode, struct file *file);
-void    my_seq_printf(struct seq_file *m, const char *fmt, ...);
-void    my_seq_puts(struct seq_file *m, const char *s);
+extern int     my_single_release(struct inode *inode, struct file *file);
+extern void    my_seq_printf(struct seq_file *m, const char *fmt, ...);
+extern void    my_seq_puts(struct seq_file *m, const char *s);
 
 #endif
